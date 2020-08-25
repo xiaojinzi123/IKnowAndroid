a9c934a19b6b0bb1832dbbc9802e0f6bd8ddece9
xiaojinzi <cxj0xiaojinzi@gmail.com>
2020-08-25 21:46:27 +0800

    升级版本库

--- LibTools/src/main/java/com/iknow/lib/tools/ContextEx.kt
+++ LibTools/src/main/java/com/iknow/lib/tools/ContextEx.kt
@@ -7,7 +7,7 @@ fun Context.isNetworkConnected(): Boolean {
     val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     // 获取NetworkInfo对象
     val networkInfo = manager.activeNetworkInfo
-    val connectivityManager: ConnectivityManager = getSystemService(
+    val _connectivityManager: ConnectivityManager = getSystemService(
         ConnectivityManager::class.java
     )
     //判断NetworkInfo对象是否为空
